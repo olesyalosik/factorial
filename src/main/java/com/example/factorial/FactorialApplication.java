5833764832f6c1a9290be452b98419ed35057fb1
Lis <losiko2004@gmail.com>
2022-11-09 10:50:27 +0300

    Initial commit

---
+++ src/main/java/com/example/factorial/FactorialApplication.java
@@ -0,0 +1,13 @@
+package com.example.factorial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FactorialApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FactorialApplication.class, args);
+	}
+
+}
