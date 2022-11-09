b725dd29a4bb2d1466e82b1b9b35400edf9a79a4
Lis <losiko2004@gmail.com>
2022-11-09 10:50:27 +0300

    Initial commit

---
+++ src/test/java/com/example/factorial/FactorialApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.factorial;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FactorialApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
